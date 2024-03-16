4bab050768dcade3ec18bef46d4d0e7bcb12e6b9
vivcolvmn <vivcolvmn@gmail.com>
2024-03-15 21:13:07 -0700

    reload functionality

--- index.js
+++ index.js
@@ -12,65 +12,119 @@ document.addEventListener("DOMContentLoaded", () => {
     main.appendChild(watercolor);
 
     watercolor.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/watercolor.png)";
         h1.style.height = "120px";
-        h1.style.width = "337px"
+        h1.style.width = "337px";
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
     const penAndInk = document.createElement("div");
     penAndInk.className = "penAndInk";
     main.appendChild(penAndInk);
 
     penAndInk.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/pen&ink.png)";
         h1.style.height = "120px";
-        h1.style.width = "387px"
+        h1.style.width = "387px";
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
     const sculpture = document.createElement("div");
     sculpture.className = "sculpture";
     main.appendChild(sculpture);
 
     sculpture.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/sculpture.png)";
         h1.style.height = "120px";
         h1.style.width = "298px"
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
     const mixedMedia = document.createElement("div");
     mixedMedia.className = "mixedMedia";
     main.appendChild(mixedMedia);
 
     mixedMedia.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/mixed_media.png)";
         h1.style.height = "120px";
-        h1.style.width = "446px"
+        h1.style.width = "446px";
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
     const digital = document.createElement("div");
     digital.className = "digital";
     main.appendChild(digital);
 
     digital.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/digital.png)";
         h1.style.height = "120px";
-        h1.style.width = "262px"
+        h1.style.width = "262px";
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
     const about = document.createElement("div");
     about.className = "about";
     main.appendChild(about);
 
     about.addEventListener("click", () => {
+        window.scrollTo(0, 0);
         h1.style.backgroundImage = "url(./resources/about.png)";
         h1.style.height = "120px";
-        h1.style.width = "202px"
+        h1.style.width = "202px";
         main.innerHTML = "";
-    })
+        const reload = document.createElement("button");
+        reload.className = "reload";
+        reload.innerText = "back"
+        main.appendChild(reload);
+
+        reload.addEventListener("click", () => {
+            location.reload();
+        })
+    });
 
 });
